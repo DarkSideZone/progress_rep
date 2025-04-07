55185acec913164279bcf4aac8a791b797654889
darkside <arzymatnarynbekuulu@gmail.com>
2025-04-07 16:33:59 +0600

    Product model

--- app/database/models.py
+++ app/database/models.py
@@ -20,6 +20,41 @@ class Category(Base):
     products = relationship("Product", back_populates="category")
 
 
+# class Stock_in(Base):
+#     __tablename__ = 'stock_in'
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     product_id = Column(Integer, ForeignKey('products.id'),nullable=False)
+#     quantity = Column(Integer, nullable=False)
+#     time = Column(DateTime, server_default=func.now(),nullable=False)
+
+#     product = relationship("Product", back_populates="stock_in_records")
+
+
+# class Stock_out(Base):
+#     __tablename__= 'stock_out'
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     product_id = Column(Integer, ForeignKey,('cotegory.id'), nullable=False)
+#     quantity = Column(Integer, nullable=False)
+#     time = Column(DateTime, server_default=func.now(), nullable=False)
+
+#     product = relationship("Product", back_populates="stock_out_records")
+    
+
+class Products(Base):
+    __tablename__= 'products'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    price = Column(Integer, nullable=False)
+    category_id = Column(Integer, ForeignKey, ('categories.id'), nullable=True)
+
+    category = relationship("Category", back_populates="products")
+    stock_in_records = relationship("StockIn", back_populates="product")
+    stock_out_records = relationship("StockOut", back_populates="product")
+
+
 async def async_main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
